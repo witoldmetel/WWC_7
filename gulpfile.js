c7b5070fae8a0aea6a8594dd1bf927e133ee6976
Managersky <witoldmetel@gmail.com>
2018-03-14 13:47:42 +0100

    gulp file edit

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,34 @@
 var gulp = require("gulp");
+
 var browserSync = require("browser-sync");
 var sass = require("gulp-sass");
 var sourcemaps = require("gulp-sourcemaps");
 var autoprefixer = require("gulp-autoprefixer");
+var cleanCSS = require("gulp-clean-css");
+var uglify = require("gulp-uglifyes");
+var concat = require("gulp-concat");
+var imagemin = require("gulp-imagemin");
+var changed = require("gulp-changed");
+var htmlReplace = require("gulp-html-replace");
+var htmlmin = require("gulp-htmlmin");
+var del = require("del");
+var sequence = require("run-sequence");
 
 var path = {
     dist: "dist/",
     src: "src/",
     cssin: "src/css/**/*.css",
+    jsin: "src/js/**/*.js",
+    imgin: "src/img/**/*.{jpg,jpeg,png,gif}",
     htmlin: "src/*.html",
     scssin: "src/scss/**/*.scss",
-    cssout: "dest/css/",
-    htmlout: "dest/",
+    fontsin: "src/fonts/**/*.*",
+    cssout: "dist/css/",
+    jsout: "dist/js/",
+    imgout: "dist/img/",
+    htmlout: "dist/",
     scssout: "src/css/",
+    fontsout: "dist/fonts/",
     cssoutname: "style.css",
     jsoutname: "script.js",
     cssplaceout: "css/style.css",
@@ -28,20 +44,72 @@ gulp.task("serve", ["sass"], function () {
         server: path.src
     });
 
-    gulp.watch([path.htmlin], ["reload"]);
+    gulp.watch([path.htmlin, path.jsin], ["reload"]);
     gulp.watch(path.scssin, ["sass"]);
 });
 
 gulp.task("sass", function () {
-    return gulp.src(path.scssin) //dzieki takiemu zapisowi mamy  dostep w podfolderach
+    return gulp.src(path.scssin)
         .pipe(sourcemaps.init())
-        .pipe(sass().on("error", sass.logError)) //jakie zadanie ma wykonac
+        .pipe(sass().on("error", sass.logError))
         .pipe(autoprefixer({
             browsers: ["last 3 versions"]
         }))
         .pipe(sourcemaps.write())
         .pipe(gulp.dest(path.scssout))
-        .pipe(browserSync.stream()); //
+        .pipe(browserSync.stream());
+});
+
+gulp.task("css", function () {
+    return gulp.src(path.cssin)
+        .pipe(concat(path.cssoutname))
+        .pipe(cleanCSS())
+        .pipe(gulp.dest(path.cssout));
+});
+
+gulp.task("js", function () {
+    return gulp.src(path.jsin)
+        .pipe(uglify().on('error', function (e) {
+            console.log(e);
+        }))
+        .pipe(concat(path.jsoutname))
+        .pipe(uglify())
+        .pipe(gulp.dest(path.jsout));
+});
+
+gulp.task("img", function () {
+    return gulp.src(path.imgin)
+        .pipe(changed(path.imgout))
+        .pipe(imagemin())
+        .pipe(gulp.dest(path.imgout));
+});
+
+gulp.task("html", function () {
+    return gulp.src(path.htmlin)
+        .pipe(htmlReplace({
+            "css": path.cssplaceout,
+            "js": path.jsplaceout
+        }))
+        .pipe(htmlmin({
+            sortAttributes: true, //sortowanie po atrybutach
+            sortClassName: true, //sortowanie po klasach
+            //oba sortowania są istotne dla gzipa - optymalizacja pod katem predkosci ladowania
+            // collapseWhitespace: true //usuwa wszystkie biale znaki
+        }))
+        .pipe(gulp.dest(path.dist));
+});
+
+gulp.task("fonts", function () {
+    return gulp.src(path.fontsin)
+        .pipe(gulp.dest(path.fontsout));
+});
+
+gulp.task("clean", function () {
+    return del([path.dist]);
+});
+
+gulp.task("build", function () {
+    sequence("clean", ["html", "js", "css", "img", "fonts"]);
 });
 
 gulp.task("default", ["serve"]);
